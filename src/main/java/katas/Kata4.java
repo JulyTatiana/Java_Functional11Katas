33b34e9345293ee209df61bba29fb74f46da1e18
julytatiana <yulytatiana@live.com>
2022-05-26 00:30:38 -0500

    Kata 4 was tested

--- src/main/java/katas/Kata4.java
+++ src/main/java/katas/Kata4.java
@@ -6,8 +6,10 @@ import model.BoxArt;
 import model.MovieList;
 import util.DataUtil;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve id, title, and a 150x200 box art url for every video
@@ -18,6 +20,25 @@ public class Kata4 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));
+        //return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));
+        return movieLists.stream()
+                .map(movieList -> movieList.getVideos())
+                .flatMap(movies -> movies.stream())
+                .map(movie -> {
+                    BoxArt boxArt1 = movie.getBoxarts().stream()
+                            .filter(boxArt -> boxArt.getWidth() == 150 && boxArt.getHeight() == 200)
+                            .findFirst()
+                            .orElseThrow();
+                    HashMap<String, String> map = new HashMap<>();
+                    map.put("title", movie.getTitle());
+                    map.put("id", movie.getId().toString());
+                    map.put("boxart", boxArt1.getUrl());
+                    return  map;
+                }).collect(Collectors.toList());
+    }
+    public static void main(String[] args){
+
+        Kata4 kata4 = new Kata4();
+        System.out.println(kata4.execute());
     }
 }
